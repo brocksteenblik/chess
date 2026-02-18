a922653cce65190c5dfe1fa9af0919701c453e3e
Brock Steenblik <brocksteenblik@gmail.com>
2026-02-17 21:57:07 -0700

    Code Quality for PawnMoveCalc

--- shared/src/main/java/chess/PawnMoveCalc.java
+++ shared/src/main/java/chess/PawnMoveCalc.java
@@ -3,13 +3,15 @@ package chess;
 import java.util.ArrayList;
 
 public class PawnMoveCalc extends PieceMovesCalculator{
-    private ArrayList<ChessMove> pawnMoves = new ArrayList<>();
+    private final ArrayList<ChessMove> pawnMoves = new ArrayList<>();
 
-    private void promote(ChessPosition myPosition, int row, int col){
-        pawnMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), ChessPiece.PieceType.ROOK));
-        pawnMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), ChessPiece.PieceType.BISHOP));
-        pawnMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), ChessPiece.PieceType.KNIGHT));
-        pawnMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), ChessPiece.PieceType.QUEEN));
+    private void promotionMoves(ChessPosition myPosition, int row, int col){
+        ChessPosition startPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        ChessPosition endPosition = new ChessPosition(row, col);
+        pawnMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.ROOK));
+        pawnMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.BISHOP));
+        pawnMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+        pawnMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.QUEEN));
     }
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece pawn = board.getPiece(myPosition);
@@ -19,7 +21,7 @@ public class PawnMoveCalc extends PieceMovesCalculator{
             if (row <= 7) {
                 if (board.getPiece(new ChessPosition(row + 1, col)) == null) {
                     if (row == 7){
-                        promote(myPosition, row + 1, col);
+                        promotionMoves(myPosition, row + 1, col);
                     }
                     else {
                         checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 1, col);
@@ -28,32 +30,14 @@ public class PawnMoveCalc extends PieceMovesCalculator{
                         checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 2, col);
                     }
                 }
-                if (col > 1) {
-                    if (board.getPiece(new ChessPosition(row + 1, col - 1)) != null) {
-                        if (row == 7){
-                            promote(myPosition, row + 1, col - 1);
-                        }
-                        else{
-                            checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 1, col - 1);
-                        }
-                    }
-                }
-                if (col < 8) {
-                    if (board.getPiece(new ChessPosition(row + 1, col + 1)) != null) {
-                        if (row == 7){
-                            promote(myPosition, row + 1, col + 1);
-                        }
-                        else{
-                            checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 1, col + 1);
-                        }                    }
-                }
+                promotionWhite(board, myPosition, col, row, pawn);
             }
         }
         if (pawn.getTeamColor() == ChessGame.TeamColor.BLACK){
             if (row >= 2) {
                 if (board.getPiece(new ChessPosition(row - 1, col)) == null) {
                     if (row == 2){
-                        promote(myPosition, row - 1, col);
+                        promotionMoves(myPosition, row - 1, col);
                     }
                     else {
                         checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 1, col);
@@ -62,27 +46,53 @@ public class PawnMoveCalc extends PieceMovesCalculator{
                         checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 2, col);
                     }
                 }
-                if (col > 1) {
-                    if (board.getPiece(new ChessPosition(row - 1, col - 1)) != null) {
-                        if (row == 2){
-                            promote(myPosition, row - 1, col - 1);
-                        }
-                        else{
-                            checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 1, col - 1);
-                        }
-                    }
-                }
-                if (col < 8) {
-                    if (board.getPiece(new ChessPosition(row - 1, col + 1)) != null) {
-                        if (row == 2){
-                            promote(myPosition, row - 1, col + 1);
-                        }
-                        else{
-                            checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 1, col + 1);
-                        }                    }
-                }
+                promotionBlack(board, myPosition, col, row, pawn);
             }
         }
         return pawnMoves;
+    }
+
+    private void promotionBlack(ChessBoard board, ChessPosition myPosition, int col, int row, ChessPiece pawn) {
+        if (col > 1) {
+            if (board.getPiece(new ChessPosition(row - 1, col - 1)) != null) {
+                if (row == 2){
+                    promotionMoves(myPosition, row - 1, col - 1);
+                }
+                else{
+                    checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 1, col - 1);
+                }
+            }
+        }
+        if (col < 8) {
+            if (board.getPiece(new ChessPosition(row - 1, col + 1)) != null) {
+                if (row == 2){
+                    promotionMoves(myPosition, row - 1, col + 1);
+                }
+                else{
+                    checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row - 1, col + 1);
+                }                    }
+        }
+    }
+
+    private void promotionWhite(ChessBoard board, ChessPosition myPosition, int col, int row, ChessPiece pawn) {
+        if (col > 1) {
+            if (board.getPiece(new ChessPosition(row + 1, col - 1)) != null) {
+                if (row == 7){
+                    promotionMoves(myPosition, row + 1, col - 1);
+                }
+                else{
+                    checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 1, col - 1);
+                }
+            }
+        }
+        if (col < 8) {
+            if (board.getPiece(new ChessPosition(row + 1, col + 1)) != null) {
+                if (row == 7){
+                    promotionMoves(myPosition, row + 1, col + 1);
+                }
+                else{
+                    checkAndAddNewSpace(board, pawnMoves, pawn, myPosition, row + 1, col + 1);
+                }                    }
+        }
     }
 }
