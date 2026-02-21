c223ef0bb633176fd29e3c74776181a17a752671
Brock Steenblik <brocksteenblik@gmail.com>
2026-02-21 16:09:16 -0700

    Created InputException class

---
+++ server/src/main/java/handler/InputException.java
@@ -0,0 +1,16 @@
+package handler;
+
+import com.google.gson.Gson;
+import java.util.Map;
+
+public class InputException extends RuntimeException {
+
+    final private int code;
+
+    public InputException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public String toJson(){return new Gson().toJson(Map.of("message", getMessage(), "status", code));}
+}
