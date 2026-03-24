760f2820fa3c4a445641bf10c8c590d3c12213c3
Brock Steenblik <brocksteenblik@gmail.com>
2026-03-24 00:05:22 -0600

    Updated help command to show postlogin UI information

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -62,15 +62,19 @@ public class Client {
 
     private String registerUser(String[] params) throws ResponseException {
         if (params.length >= 3){
-            state = State.LOGGED_IN;
             String username = params[0];
             String password = params[1];
             String email = params[2];
             RegisterResult registerResult = server.userRegistration(username, password, email);
-            return String.format("You signed up as %s", registerResult.username()
-            );
+            if (registerResult.username() != null){
+                state = State.LOGGED_IN;
+                return String.format("You signed up as %s", registerResult.username());
+            }
+            else{
+                return "That username is already taken. Try a new username!";
+            }
         }
-        return "Try again!";
+        return "Too few parameters. Make sure to include a username, a password, and an email!";
     }
 
     private String login(String[] params) {
@@ -106,13 +110,23 @@ public class Client {
         setColor("YELLOW");
         if (state == State.LOGGED_OUT){
             return """
-                    help - Display info about what actions can be taken.
-                    quit - Exit the program.
                     register <USERNAME> <PASSWORD> <EMAIL> - Provide info to create an account.
                     login <USERNAME> <PASSWORD> - Provide info to login to existing account.
+                    help - Display info about what actions can be taken.
+                    quit - Exit the program.
                     """;
         }
-        return null;
+        else{
+            return """
+                    create <NAME> - Create a new game.
+                    list - Show all games in database.
+                    join <ID> [WHITE|BLACK] - Choose a game to play and which color to play as.
+                    observe <ID> - Watch an ongoing game.
+                    logout - Logs current user out.
+                    quit - Exit the program.
+                    help - Display info about what actions can be taken.
+                    """;
+        }
     }
 
     private void setColor(String color){
