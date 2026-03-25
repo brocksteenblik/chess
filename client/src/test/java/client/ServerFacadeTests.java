ca1dc8664db5a44b31ee5cc4c33178618b0c520e
Brock Steenblik <brocksteenblik@gmail.com>
2026-03-25 13:13:18 -0600

    Code quality fix

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -186,7 +186,8 @@ public class ServerFacadeTests {
             facade.userPlayGame(games.getFirst().gameID(), "white", registerResult.authToken());
             facade.userLogout(registerResult.authToken());
             var registerResult2 = facade.userRegistration("evilman", "5678", "email@emails.com");
-            Assertions.assertThrows(ResponseException.class, ()->facade.userPlayGame(games.getFirst().gameID(), "white", registerResult2.authToken()));
+            Assertions.assertThrows(ResponseException.class,
+                    ()->facade.userPlayGame(games.getFirst().gameID(), "white", registerResult2.authToken()));
         } catch (ResponseException e) {
             throw new RuntimeException(e);
         }
