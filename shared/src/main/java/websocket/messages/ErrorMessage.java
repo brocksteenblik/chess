ece58a075dcd724e1db8c4eced52dd6bb15344ea
Brock Steenblik <brocksteenblik@gmail.com>
2026-04-06 22:13:46 -0600

    Created new ServerMessage extensions

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,21 @@
+package websocket.messages;
+
+import com.google.gson.Gson;
+
+public class ErrorMessage extends ServerMessage{
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,22 @@
+package websocket.messages;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+
+public class LoadGameMessage extends ServerMessage{
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame(){
+        return game;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
